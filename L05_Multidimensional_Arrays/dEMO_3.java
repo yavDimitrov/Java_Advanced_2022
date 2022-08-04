2ddcbea31366974f8847754ecfc85d644c01c0fe
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-08-04 08:49:27 +0300

    L05_Multidimentional_Arrays_Demo03

---
+++ L05_Multidimensional_Arrays/dEMO_3.java
@@ -0,0 +1,35 @@
+package LECTURES.L05_Multidimensional_Arrays;
+
+import java.util.Scanner;
+
+public class dEMO_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        /*int n = Integer.parseInt(scan.nextLine());
+
+        int [] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(scan.nextLine());
+
+        }
+
+        int middleIndex = arr.length/2;
+
+        int middleElenent = arr[middleIndex];
+
+        System.out.println(middleElenent);*/
+
+        int[] arr1 = {};
+        int[] arr2 = {13,42,69};
+        int[] arr3 = new int[2];
+
+        int [][]matrix = {
+                {13,42,69},
+                {13,42,69},
+                {13,42,69}
+        };
+
+    }
+}
