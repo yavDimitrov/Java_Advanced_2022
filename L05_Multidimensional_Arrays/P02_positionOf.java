c85ca6a6223ef74973f690e86865185fdac8e486
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-08-06 10:24:57 +0300

    L05_Multidimentional_Arrays_P02

---
+++ L05_Multidimensional_Arrays/P02_positionOf.java
@@ -0,0 +1,43 @@
+package LECTURES.L05_Multidimensional_Arrays;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class P02_positionOf {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int [][] matrix = Utils_J_05.readMatrix2D(scan);
+
+        int number = (Integer.parseInt(scan.nextLine()));
+
+        ArrayList<String> output = new ArrayList<>();
+
+        for (int row = 0; row < matrix.length; row++) {
+            for (int col = 0; col < matrix[row].length; col++) {
+                if(matrix[row][col] == number){
+                    output.add(row + " " + col);
+                }
+
+            }
+
+        }
+
+        String outPut = formatOutput(output) ;
+
+        System.out.println(outPut);
+
+
+        }
+
+    private static String formatOutput(ArrayList<String> output) {
+        if(output.isEmpty()){
+            return "not found";
+        }
+        StringBuilder out = new StringBuilder();
+        for (String outputLines:output) {
+                out.append(outputLines).append(System.lineSeparator());
+    }
+        return out.toString().trim();
+}
+}
