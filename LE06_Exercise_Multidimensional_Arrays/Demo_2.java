88aa276f78f1d06d99b6721044d272139c17e03b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-08-10 22:33:34 +0300

    LE06_Exercise_Multidimensional_Arrays_Demo_02

---
+++ LE06_Exercise_Multidimensional_Arrays/Demo_2.java
@@ -0,0 +1,23 @@
+package EXERCISES.LE06_Exercise_Multidimensional_Arrays;
+
+import java.util.Scanner;
+
+public class Demo_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int rows = 2;
+        int cols = 3;
+        int [][] matrix = new int [rows][cols];
+
+        for (int row = 0; row < rows; row++) {
+            String [] rowInput = scan.nextLine().split(" ");
+
+        }
+
+
+
+
+
+    }
+}
