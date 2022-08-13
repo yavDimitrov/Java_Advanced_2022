0a784264faf4bee3c014291cf64bd2f45b9b3a0b
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-08-13 09:17:19 +0300

    LE06_Exercise_Multidimensional_Arrays_P05

---
+++ LE06_Exercise_Multidimensional_Arrays/P05_matrixSufling.java
@@ -0,0 +1,9 @@
+package EXERCISES.LE06_Exercise_Multidimensional_Arrays;
+
+import java.util.Scanner;
+
+public class P05_matrixSufling {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+    }
+}
