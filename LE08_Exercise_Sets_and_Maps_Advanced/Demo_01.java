1454c3d629e7ca1adbb343cd317b41e1b4cbfd52
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-08-22 08:30:56 +0300

    LE08_Exercise_Sets_and_Maps_Advanced

---
+++ LE08_Exercise_Sets_and_Maps_Advanced/Demo_01.java
@@ -0,0 +1,19 @@
+package EXERCISES.LE08_Exercise_Sets_and_Maps_Advanced;
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Demo_01 {
+    public static void main(String[] args) {
+
+        HashMap<String, Integer> map = new HashMap<>();
+        map.put("Pesho", 6);
+        map.put("Ivan", 6);
+        map.put("Georgi", 6);
+        map.put("Andrey", 5);
+
+        System.out.println();
+
+        HashSet<String> set = new HashSet<>();
+    }
+}
