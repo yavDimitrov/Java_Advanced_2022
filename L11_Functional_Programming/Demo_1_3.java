977679df50a1204aee3851a6581c3dae0f742f37
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-06 22:54:39 +0300

    L11_Functional_Programming_3

---
+++ L11_Functional_Programming/Demo_1_3.java
@@ -0,0 +1,38 @@
+package LECTURES.L11_Functional_Programming;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Demo_1_3 {
+    public static void main(String[] args) throws IOException {
+
+        //Method:
+        //return type -> double
+        // name -> getPowerOf
+        // parameters list -> (int x, int pow)
+        // signature -> getPowerOf(int, int)
+        // body -> {return Math.pow(x, pow);}
+
+        //Read numebrs arr[] from consolle on a line
+        //Read number pow
+        //Raise and print all numbers from arr[] to pow
+
+        int [] arr = {13, 42, 69, 73};
+        int pow = 2;
+
+        Arrays.stream(arr)
+                .mapToDouble(n -> getPowerOf(n, pow))
+                .forEach(System.out::println);
+
+     /*   for (int number : arr) {
+            double powerOf = getPowerOf(number, pow);
+            System.out.println(powerOf);
+        } */
+
+        
+    }
+    public static double getPowerOf(int x, int pow){
+
+        return Math.pow(x, pow);
+    }
+}
