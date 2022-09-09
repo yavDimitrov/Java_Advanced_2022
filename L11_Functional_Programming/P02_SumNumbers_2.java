01e8ebcb5c62afb962cf31c345a6aea423206d53
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-09 22:08:15 +0300

    L11_Functional_Programming_P02_2

---
+++ L11_Functional_Programming/P02_SumNumbers_2.java
@@ -0,0 +1,46 @@
+package LECTURES.L11_Functional_Programming;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class P02_SumNumbers_2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<Integer> numbers = Arrays.stream(scan.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+
+//        Count = 10
+//        Sum = 41
+
+        //System.out.println("Count = " + numbers.size());
+
+        Function<List<Integer>, String> countFormatter = list -> "Count = " + list.size();
+
+        Function<List<Integer>, Integer> sumAllElements = l -> {
+            Stream<Integer> stream = l.stream();
+
+            IntStream intStream = stream.mapToInt(e -> e);
+
+            return intStream.sum();
+        };
+
+        Function<Integer, String> sumFormatter = s -> "Sum = " + s;
+
+        String countOutput = countFormatter.apply(numbers);
+
+        System.out.println(countOutput);
+
+        int sum = sumAllElements.apply(numbers);
+        String sumOutput = sumFormatter.apply(sum);
+
+        System.out.println(sumOutput);
+    }
+}
