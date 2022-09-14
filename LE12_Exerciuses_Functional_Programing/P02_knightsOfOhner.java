c5f8f16ad2a69c7c70a98aa457a5602107f6d3ba
Yavor Dimitrov <yav.dimitrov@gmail.com>
2022-09-14 07:17:52 +0300

    LE12_Exerciuses_Functional_Programing_p02

---
+++ LE12_Exerciuses_Functional_Programing/P02_knightsOfOhner.java
@@ -0,0 +1,20 @@
+/*
+package EXERCISES.LE12_Exerciuses_Functional_Programing;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class P02_knightsOfHonor {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        List<String> names = Arrays.stream(scan.nextLine().split("\\s+")).collect(Collectors.toList());
+
+        names.stream().forEach(name -> System.out.printf("Sir %s%n", name));
+
+
+    }
+}
+*/
